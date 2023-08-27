e791eb1aa35121c9d6819f8bf133224e813c988a
Faihaan Arif <farif1234@gmail.com>
2023-08-27 01:17:05 -0700

    Add transparent overlay

--- content.js
+++ content.js
@@ -32,32 +32,45 @@ button.style.position = "fixed";
 button.style.top = "100px";
 button.style.right = "10px";
 button.textContent = "Open Whiteboard";
-button.style.zIndex = "100"; // Ensure the button is on top of everything
+button.style.zIndex = "700"; // Ensure the button is on top of everything
 
 // Add button to the page
 document.body.appendChild(button);
 
 // Create overlay div (whiteboard)
-const overlay = document.createElement("div");
-overlay.id = "leetcode-overlay";
-overlay.style.position = "fixed";
-overlay.style.top = "50%";
-overlay.style.left = "50%";
-overlay.style.width = "500px"; // Set the width to 500px
-overlay.style.height = "500px"; // Set the height to 500px
-overlay.style.transform = "translate(-50%, -50%)";
-overlay.style.background = "white";
-overlay.style.border = "2px solid black";
-overlay.style.zIndex = "9999"; // Ensure the overlay is on top of everything
-overlay.style.display = "none"; // Start with overlay hidden
+// const overlay = document.createElement("div");
+// overlay.id = "leetcode-overlay";
+// overlay.style.position = "fixed";
+// overlay.style.top = "50%";
+// overlay.style.left = "50%";
+// overlay.style.width = "500px"; // Set the width to 500px
+// overlay.style.height = "500px"; // Set the height to 500px
+// overlay.style.transform = "translate(-50%, -50%)";
+// // overlay.style.background = "trans";
+// overlay.style.border = "2px solid black";
+// overlay.style.zIndex = "10"; // Ensure the overlay is on top of everything
+// overlay.style.display = "none"; // Start with overlay hidden
+
+const canvas = document.createElement("canvas");
+canvas.id = "whiteboard";
+// Set canvas properties and styles as needed
+canvas.style.position = "fixed"; // or any positioning you prefer
+canvas.style.top = "0";
+canvas.style.left = "0";
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+canvas.style.zIndex = "500";
+canvas.style.display = "none";
+
+// overlay.appendChild(canvas);
 
 // Add overlay only if it doesn't already exist
-if (!document.getElementById("leetcode-overlay")) {
-    document.body.appendChild(overlay);
+if (!document.getElementById("whiteboard")) {
+    document.body.appendChild(canvas);
 }
 
 // Handle button click event
 button.addEventListener("click", () => {
     // Toggle overlay visibility
-    overlay.style.display = overlay.style.display === "none" ? "block" : "none";
+    canvas.style.display = canvas.style.display === "none" ? "block" : "none";
 });
--- overlay.js
+++ overlay.js
@@ -1,25 +1,99 @@
-const canvas = document.getElementById("whiteboard");
-const context = canvas.getContext("2d");
+var myCanvas = document.getElementById("whiteboard");
+var ctx = myCanvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// Fill Window Width and Height
+myCanvas.width = window.innerWidth;
+myCanvas.height = window.innerHeight;
 
-let drawing = false;
+// Set Background Color
+// ctx.fillStyle = "#fff";
+// ctx.fillRect(0, 0, myCanvas.width, myCanvas.height);
 
-canvas.addEventListener("mousedown", (event) => {
-    drawing = true;
-    context.beginPath();
-    context.moveTo(event.clientX, event.clientY);
-});
+// Mouse Event Handlers
+if (myCanvas) {
+    var isDown = false;
+    var canvasX, canvasY;
+    ctx.lineWidth = 5;
 
-canvas.addEventListener("mousemove", (event) => {
-    if (!drawing) return;
+    myCanvas.addEventListener("mousedown", function (e) {
+        isDown = true;
+        ctx.beginPath();
+        canvasX = e.pageX - myCanvas.offsetLeft;
+        canvasY = e.pageY - myCanvas.offsetTop;
+        ctx.moveTo(canvasX, canvasY);
+    });
 
-    context.lineTo(event.clientX, event.clientY);
-    context.stroke();
-});
+    myCanvas.addEventListener("mousemove", function (e) {
+        if (isDown !== false) {
+            canvasX = e.pageX - myCanvas.offsetLeft;
+            canvasY = e.pageY - myCanvas.offsetTop;
+            ctx.lineTo(canvasX, canvasY);
+            ctx.strokeStyle = "#000";
+            ctx.stroke();
+        }
+    });
 
-canvas.addEventListener("mouseup", () => {
-    drawing = false;
-    context.closePath();
-});
+    myCanvas.addEventListener("mouseup", function (e) {
+        isDown = false;
+        ctx.closePath();
+    });
+
+    // $(myCanvas)
+    //     .mousedown(function (e) {
+    //         isDown = true;
+    //         ctx.beginPath();
+    //         canvasX = e.pageX - myCanvas.offsetLeft;
+    //         canvasY = e.pageY - myCanvas.offsetTop;
+    //         ctx.moveTo(canvasX, canvasY);
+    //     })
+    //     .mousemove(function (e) {
+    //         if (isDown !== false) {
+    //             canvasX = e.pageX - myCanvas.offsetLeft;
+    //             canvasY = e.pageY - myCanvas.offsetTop;
+    //             ctx.lineTo(canvasX, canvasY);
+    //             ctx.strokeStyle = "#000";
+    //             ctx.stroke();
+    //         }
+    //     })
+    //     .mouseup(function (e) {
+    //         isDown = false;
+    //         ctx.closePath();
+    //     });
+}
+
+// // Touch Events Handlers
+// draw = {
+//     started: false,
+//     start: function (evt) {
+//         ctx.beginPath();
+//         ctx.moveTo(evt.touches[0].pageX, evt.touches[0].pageY);
+
+//         this.started = true;
+//     },
+//     move: function (evt) {
+//         if (this.started) {
+//             ctx.lineTo(evt.touches[0].pageX, evt.touches[0].pageY);
+
+//             ctx.strokeStyle = "#000";
+//             ctx.lineWidth = 5;
+//             ctx.stroke();
+//         }
+//     },
+//     end: function (evt) {
+//         this.started = false;
+//     },
+// };
+
+// // Touch Events
+// myCanvas.addEventListener("touchstart", draw.start, false);
+// myCanvas.addEventListener("touchend", draw.end, false);
+// myCanvas.addEventListener("touchmove", draw.move, false);
+
+// // Disable Page Move
+// document.body.addEventListener(
+//     "touchmove",
+//     function (evt) {
+//         evt.preventDefault();
+//     },
+//     false
+// );
